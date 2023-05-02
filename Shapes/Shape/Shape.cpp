f560d724404a84c22f337ad54f50b0cc08f65301
dxmdeveloper-zut <callofmortalkombat@gmail.com>
2023-05-02 21:01:57 +0200

    Shape matrix allocation hotfix

--- Shapes/Shape/Shape.cpp
+++ Shapes/Shape/Shape.cpp
@@ -8,10 +8,14 @@ static inline void matrix_uniqptr_ondelete(size_t height, bool** arr){
 
 Shape::Shape(size_t mat_width, size_t mat_height, RGB color, char symbol) 
     : mat_width{mat_width}, mat_height{mat_height}, color{color}, symbol{symbol}{
+    // uniq ptr with custom ondelete function
     this->matrix = std::unique_ptr<bool*[], std::function<void(bool**)> >(
         new bool*[mat_height](),
-        [mat_height](bool** arr) {matrix_uniqptr_ondelete(mat_height, arr);}
-);
+        [mat_height](bool** arr) {matrix_uniqptr_ondelete(mat_height, arr);});
+
+    for(size_t h = 0; h < mat_height; h++){
+        this->matrix[h] = new bool[mat_width]();
+    }
 
 }
 
